354906086f9f28da74e59447dc7b69be5d75823a
CSScooby <eliot.cousseau@gmail.com>
2025-04-27 16:17:23 +0200

    Ajout de plusieur type (pas que resto) ajout des json (version pas encore testé)

--- app.py
+++ app.py
@@ -17,26 +17,29 @@ def index():
             for step in leg['steps']:
                 lat = step['end_location']['lat']
                 lng = step['end_location']['lng']
-                found = get_restaurants(lat, lng)
+                found = get_something(lat, lng, 'restaurant')
+                if found:
+                    for restaurant in found:
+                        # Vérifier si le restaurant est déjà dans la liste
+                        if restaurant not in restos:
+                            restos.append(restaurant)
                 restos.extend(found)
         
         return render_template('results.html', restos=restos)
     
     return render_template('index.html')
 
-GOOGLE_API_KEY = 'API'  # Remplace par ta clé API
+GOOGLE_API_KEY = '  # Remplace par ta clé API
 
 def get_route(depart, arrivee):
     url = f"https://maps.googleapis.com/maps/api/directions/json?origin={depart}&destination={arrivee}&key={GOOGLE_API_KEY}"
     response = requests.get(url)
-    data = response.json()
-    return data
+    chemin = response.json()
+        # Sauvegarder dans un fichier
+    with open('route_debug.json', 'w', encoding='utf-8') as f:
+        json.dump(chemin, f, ensure_ascii=False, indent=4)
+    return chemin
 
-def get_restaurants(lat, lng):
-    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lng}&radius=2000&type=restaurant&key={GOOGLE_API_KEY}"
-    response = requests.get(url)
-    data = response.json()
-    return data['results']
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ food.py
@@ -0,0 +1,17 @@
+import requests
+from flask import Flask, render_template, request
+import types.json as all_types
+import json
+
+
+
+
+
+
+def get_something(lat, lng,type):
+    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lng}&radius=2000&type={type}&key={GOOGLE_API_KEY}"
+    response = requests.get(url)
+    type = response.json()
+    with open('restaurants_debug.json', 'w', encoding='utf-8') as f:
+        json.dump(type, f, ensure_ascii=False, indent=4)
+    return type['results']
